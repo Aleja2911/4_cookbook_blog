f1b98e56df4b7c09b55f8af567b0f3be3b51c0bc
Alejandra Ossa Jimenez <alejandra.ossa.jimenez@gmail.com>
2020-10-09 17:34:34 +0200

    blog page first draft

--- src/pages/BlogLandingPage.jsx
+++ src/pages/BlogLandingPage.jsx
@@ -12,12 +12,8 @@ const BlogLandingPage = ({ blogData }) => {
 		<div>
 			<JumbotronComponent />
 			<h1> The Chronicles of Cheese </h1>
-			<BlogEntryPreviews blogData={blogData} />
-			<Switch>		
-				<Route path="/blog-detail/{title}" component={BlogDetail}>
-					
-				</Route>
-			</Switch>
+			<BlogEntryPreviews blogData={} />
+			
 		</div>
 	);
 };
